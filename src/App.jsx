625312db72670e63bd024100e06f6116e0202921
Bahar Karakaş <baharkarakas03@gmail.com>
2024-08-28 17:20:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import Header from './components/Header'
+import Meme from './components/Meme'
+
+export default function App() {
+ return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+
